8b5e039cc76d8915bf10375b0e4cdfc6035ae8a3
VABIII <tbengtson82@gmail.com>
2022-01-28 05:00:30 -0600

    codegrade tweak

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -3,7 +3,7 @@ const db = require('../../data/dbConfig');
 
 const checkBody = (req, res, next) => {
     const { username, password } = req.body;
-    if(!username.trim() || !password) {
+    if(!username || !password) {
         next({status: 401, message: `username and password required`});
     } else { next() }
 };
--- api/server.test.js
+++ api/server.test.js
@@ -1,22 +1,3 @@
-const db = require('../data/dbConfig');
-
-beforeAll(async () => {
-  await db.migrate.rollback()
-  await db.migrate.latest()
-});
-
-beforeEach(async () => {
-  await db.seed.run()
-})
-
-beforeEach(async () => {
-  await db('users').truncate()
-})
-
-afterAll(async () => {
-  await db.destroy()
-})
-
 test('NODE_ENV is correct', () => {
   expect(process.env.NODE_ENV).toBe('testing')
 })
