62a39d748c22b4571244d70651e0baa24cf6e0c8
VABIII <tbengtson82@gmail.com>
2022-01-28 01:10:38 -0600

    first test passing

--- api/server.test.js
+++ api/server.test.js
@@ -2,23 +2,48 @@ const request = require('supertest');
 const server = require('./server');
 const db = require('../data/dbConfig');
 
+const user1 = {
+  name: 'Vern',
+  password: '1234'
+}
+const user2 = {
+  name: 'Trey',
+  password: '1234'
+}
+const user3 = {
+  name: 'Alfred',
+  password: '1234'
+}
+
+
+
 beforeAll(async () => {
   await db.migrate.rollback()
   await db.migrate.latest()
 });
 
-beforeEach(async () => {
-  await db.seed.run()
-})
+// beforeEach(async () => {
+//   await db.seed.run()
+// })
 
 afterAll(async () => {
   await db.destroy()
 })
 
+describe('POST /register', () => {
+  test('returns status 200 OK', async () => {
+    const res = await request(server)
+        .post("/register")
+        .send(user1)
+    expect(res.status).toBe(200)
+
+  })
+
+})
 
 
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true)
 })
 
 
