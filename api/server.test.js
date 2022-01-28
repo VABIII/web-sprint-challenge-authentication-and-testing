bb1e6d80701929dc5edbd955f9e188df4ca19551
VABIII <tbengtson82@gmail.com>
2022-01-28 02:02:09 -0600

    2 tests written and passing for register endpoint

--- api/server.test.js
+++ api/server.test.js
@@ -1,17 +1,18 @@
 const request = require('supertest');
 const server = require('./server');
 const db = require('../data/dbConfig');
+const User = require('../api/auth/auth-model');
 
 const user1 = {
-  name: 'Vern',
+  username: 'Vern',
   password: '1234'
 }
 const user2 = {
-  name: 'Trey',
+  username: 'Trey',
   password: '1234'
 }
 const user3 = {
-  name: 'Alfred',
+  username: 'Alfred',
   password: '1234'
 }
 
@@ -26,6 +27,10 @@ beforeAll(async () => {
 //   await db.seed.run()
 // })
 
+beforeEach(async () => {
+  await db('users').truncate()
+})
+
 afterAll(async () => {
   await db.destroy()
 })
@@ -33,14 +38,21 @@ afterAll(async () => {
 describe('POST /register', () => {
   test('returns status 200 OK', async () => {
     const res = await request(server)
-        .post("/register")
+        .post('/register')
         .send(user1)
     expect(res.status).toBe(200)
-
   })
-
 })
 
+describe('POST /register', () => {
+  test('returns username', async () => {
+    const res = await  request(server)
+        .post('/register')
+        .send(user2)
+    expect(res.body).toBeTruthy()
+
+  })
+})
 
 test('sanity', () => {
   expect(true).toBe(true)
